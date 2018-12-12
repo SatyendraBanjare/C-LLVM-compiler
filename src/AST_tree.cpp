bd6749d77f6749c0f323ead1229a2ed0e1bcd86d
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-12-12 23:48:46 +0530

    (src): update

--- src/AST_tree.cpp
+++ src/AST_tree.cpp
@@ -1,1 +1,76 @@
 #include "AST_tree.h"
+// #include the output file of flex and bison
+
+Value* IntExprNode::codegen(){
+
+}
+
+Value* CharExprNode::codegen(){
+
+}
+
+Value* DoubleExprNode::codegen(){
+
+}
+
+Value* VariableExprNode::codegen(){
+
+}
+
+Value* OperatorExprNode::codegen(){
+
+}
+
+Value* BlockExprNode::codegen(){
+
+}
+
+Value* AssignExprNode::codegen(){
+
+}
+
+Value* FuncExprNode::codegen(){
+
+}
+
+Value* CastExprNode::codegen(){
+
+}
+
+Value* ExprStatementNode::codegen(){
+
+}
+
+Value* ReturnStatementNode::codegen(){
+
+}
+
+Value* VarDecStatementNode::codegen(){
+
+}
+
+Value* ArrayDecStatementNode ::codegen(){
+
+}
+
+Value* IndexExprNode::codegen(){
+
+}
+
+Value* FuncDecStatementNode::codegen(){
+
+}
+
+Value* ExternFuncDecStatementNode::codegen(){
+
+}
+
+Value* IfStatementNode::codegen(){
+
+}
+
+Value* ForStatementNode::codegen(){
+
+}
+
+
