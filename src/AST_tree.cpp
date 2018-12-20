07a3c2cee55f2a5b8a997459d4746c0d1164b8f4
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-12-21 01:42:17 +0530

    initialized empty functions

--- src/AST_tree.cpp
+++ src/AST_tree.cpp
@@ -1,75 +1,97 @@
 #include "AST_tree.h"
 // #include the output file of flex and bison
 
-Value* IntExprNode::codegen(){
+static LLVMContext TheContext;
+static IRBuilder<> Builder(TheContext);
+static std::unique_ptr<Module> TheModule;
+static std::map<std::string, AllocaInst *> NamedValues;
+static std::unique_ptr<KaleidoscopeJIT> TheJIT;
+static std::map<std::string, std::unique_ptr<PrototypeAST>> FunctionProtos;
+
+// Function to print errors for debugging.
+Value *LogErrorV(const char *Str) {
+  LogError(Str);
+  return nullptr;
+}
+
+/// CreateEntryBlockAlloca - Create an alloca instruction in the entry block of
+/// the function.  This is used for mutable variables etc.
+static AllocaInst *CreateEntryBlockAlloca(Function *TheFunction,
+                                          const std::string &VarName) {
+  IRBuilder<> TmpB(&TheFunction->getEntryBlock(),
+                   TheFunction->getEntryBlock().begin());
+  return TmpB.CreateAlloca(Type::getDoubleTy(TheContext), nullptr, VarName);
+}
+
+static Value* IntExprNode::codegen(){
 
 }
 
-Value* CharExprNode::codegen(){
+static Value* CharExprNode::codegen(){
 
 }
 
-Value* DoubleExprNode::codegen(){
+static Value* DoubleExprNode::codegen(){
 
 }
 
-Value* VariableExprNode::codegen(){
+static Value* VariableExprNode::codegen(){
 
 }
 
-Value* OperatorExprNode::codegen(){
+static Value* OperatorExprNode::codegen(){
 
 }
 
-Value* BlockExprNode::codegen(){
+static Value* BlockExprNode::codegen(){
+`
+}
+
+static Value* AssignExprNode::codegen(){
 
 }
 
-Value* AssignExprNode::codegen(){
+static Value* FuncExprNode::codegen(){
 
 }
 
-Value* FuncExprNode::codegen(){
+static Value* CastExprNode::codegen(){
 
 }
 
-Value* CastExprNode::codegen(){
+static Value* ExprStatementNode::codegen(){
 
 }
 
-Value* ExprStatementNode::codegen(){
+static Value* ReturnStatementNode::codegen(){
 
 }
 
-Value* ReturnStatementNode::codegen(){
+static Value* VarDecStatementNode::codegen(){
 
 }
 
-Value* VarDecStatementNode::codegen(){
+static Value* ArrayDecStatementNode ::codegen(){
 
 }
 
-Value* ArrayDecStatementNode ::codegen(){
+static Value* IndexExprNode::codegen(){
 
 }
 
-Value* IndexExprNode::codegen(){
+static Value* FuncDecStatementNode::codegen(){
 
 }
 
-Value* FuncDecStatementNode::codegen(){
+static Value* ExternFuncDecStatementNode::codegen(){
 
 }
 
-Value* ExternFuncDecStatementNode::codegen(){
+static Value* IfStatementNode::codegen(){
 
 }
 
-Value* IfStatementNode::codegen(){
-
-}
-
-Value* ForStatementNode::codegen(){
+static Value* ForStatementNode::codegen(){
 
 }
 
