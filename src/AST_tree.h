a3b742b60a962a866560b122d525cffc0ff82a97
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2019-03-12 12:21:17 +0530

    added the final header definitions

--- src/AST_tree.h
+++ src/AST_tree.h
@@ -1,175 +1,334 @@
 #ifndef AST_TREE_H
 #define AST_TREE_H
 
-#include "llvm/ADT/APFloat.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/Verifier.h"
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Target/TargetMachine.h"
-#include "llvm/Transforms/InstCombine/InstCombine.h"
-#include "llvm/Transforms/Scalar.h"
-#include "llvm/Transforms/Scalar/GVN.h"
-
-#include "llvm/Bitcode/ReaderWriter.h"
-#include "llvm/Support/MemoryBuffer.h"
-#include <llvm/ExecutionEngine/GenericValue.h>
-#include <llvm/ExecutionEngine/Interpreter.h>
-#include <llvm/ExecutionEngine/MCJIT.h>
-#include <llvm/Support/TargetSelect.h>
-#include <llvm/Support/Host.h>
-#include <llvm/Support/raw_ostream.h>
-#include <llvm/IR/LLVMContext.h>
-#include "llvm/IR/Verifier.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Verifier.h"
-
 #include <iostream>
 #include <sstream>
 #include <fstream>
-#include <typeinfo>
-#include <algorithm>
-#include <cassert>
-#include <cctype>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <map>
-#include <memory>
-#include <string>
 #include <vector>
+#include <stack>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <typeinfo>
 
-// pretty messed up, figuring out what I will need finally.
-// Leaving it as it is for now.
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/CallingConv.h>
+#include <llvm/IR/IRPrintingPasses.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/GlobalVariable.h>
+#include <llvm/IR/InlineAsm.h>
+#include <llvm/Bitcode/ReaderWriter.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/FormattedStream.h>
+#include <llvm/Support/MathExtras.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/ADT/SmallVector.h>
+#include <llvm/Pass.h>
 
 using namespace llvm;
 using namespace std;
 
-enum Valid_Type {
-    V_UNKNOWN = -1,
-    V_VOID = 0,
-    V_CONST,
-    V_CHAR,
-    V_INT,
-    V_FLOAT,
-    V_PTR,
-    V_FUNC,
+enum E_TYPE {
+    E_UNKNOWN = -1,
+    E_VOID = 0,
+    E_CONST,
+    E_CHAR,
+    E_INT,
+    E_DOUBLE,
+    E_PTR,
+    E_FUNC,
 };
 
-class RootAST {
+class GenBlock {
 public:
-    RootAST() {};
-    virtual ~RootAST(){};
-    virtual Value *codegen() = 0;
+    BasicBlock *block;
+    map<string, Value*> locals;
 };
 
-class ExprNode: public RootAST {
+class BlockExprNode;
+
+class GenContext {
 public:
-    Valid_Type _type;
+    stack<GenBlock *> blocks;
+    map<string, Value*> globalVariables;
+    Function *mainFunction;
+    Function *tempFunction;
+    string code;
+    bool funcDeclaring;
+    Module *module;
+public:
+    GenContext() {
+        funcDeclaring = false;
+        module = new Module("main", getGlobalContext());
+    }
+    void CodeGen(BlockExprNode& root);
+    void OutputCode(ostream &out);
+    GenericValue run();
+    map<string, Value*>& locals()                   { return blocks.top()->locals; }
+    map<string, Value*>& globals()                  { return globalVariables; }
+    BasicBlock *context()                           { return blocks.top()->block; }
+    Function* currentFunction()                     { return tempFunction; }
+    void currentFunction(Function *function)        { tempFunction = function; }
+    void ret(BasicBlock* block)                     { blocks.top()->block = block; }
+    void push(BasicBlock *block, bool copy_locals = true) {
+        GenBlock* new_block = new GenBlock();
+        new_block->block = block;
+        if(copy_locals) {
+            map<string, Value*> prev_locals = map<string, Value*>(blocks.top()->locals);
+            new_block->locals = prev_locals;
+        }
+        blocks.push(new_block);
+    }
+    void pop() {
+        GenBlock *top = blocks.top();
+        blocks.pop();
+        delete top;
+    }
 };
 
-class StatementNode: public RootAST {};
+
+
+class ASTNode {
+public:
+    ASTNode() {};
+    virtual ~ASTNode(){};
+    virtual Value *CodeGen(GenContext&) = 0;
+    
+};
+
+class ExprNode: public ASTNode {
+public:
+    E_TYPE _type;
+};
+
+class StatementNode: public ASTNode {};
 
 class IntExprNode: public ExprNode {
 public:
-    virtual Value *codegen();
+    long long val;
+public:
+    IntExprNode(long long val): val(val) {
+        _type = E_INT;
+    }
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class CharExprNode: public ExprNode {
 public:
-    virtual Value *codegen();
+    char val;
+public:
+    CharExprNode(char val): val(val) {
+        _type = E_CHAR;
+    }
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class DoubleExprNode: public ExprNode {
+    double val;
 public:
-    virtual Value *codegen();
+    DoubleExprNode(double val): val(val) {
+        _type = E_DOUBLE;
+    }
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class VariableExprNode: public ExprNode {
 public:
-    virtual Value *codegen();
+    string name;
+public:
+    VariableExprNode(const string &name, E_TYPE type = E_UNKNOWN): name(name) {
+        _type = type;
+    }
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class OperatorExprNode: public ExprNode {
 public:
-    virtual Value *codegen();
+    int op;
+    ExprNode *left, *right;
+public:
+    OperatorExprNode(ExprNode *left, int op, ExprNode *right): left(left), right(right), op(op) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class BlockExprNode: public ExprNode {
 public:
-    virtual Value *codegen();
+    vector<StatementNode*> *statements;
+public:
+    BlockExprNode(): statements(new vector<StatementNode*>()) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class AssignExprNode: public ExprNode {
 public:
-    virtual Value *codegen();
+    VariableExprNode *left;
+    ExprNode *right;
+public:
+    AssignExprNode(VariableExprNode *left, ExprNode *right): left(left), right(right) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class FuncExprNode: public ExprNode {
 public:
-    virtual Value *codegen();
+    VariableExprNode *functor;
+    vector<ExprNode*> *args;
+public:
+    FuncExprNode(VariableExprNode *functor): functor(functor), args(new vector<ExprNode*>()) {}
+    FuncExprNode(VariableExprNode *functor, vector<ExprNode*> *args): functor(functor), args(args) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class CastExprNode: public ExprNode {
 public:
-    virtual Value *codegen();
+    VariableExprNode *type;
+    ExprNode *expr;
+public:
+    CastExprNode(VariableExprNode *type, ExprNode *expr): type(type), expr(expr) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class ExprStatementNode : public StatementNode {
 public:
-    virtual Value *codegen();
+    ExprNode *expr;
+public:
+    ExprStatementNode(ExprNode *expr): expr(expr) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class ReturnStatementNode : public StatementNode {
 public:
-    virtual Value *codegen();
+    ExprNode *expr;
+public:
+    ReturnStatementNode(ExprNode *expr): expr(expr) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class VarDecStatementNode : public StatementNode {
 public:
-    virtual Value *codegen();
+    VariableExprNode *type;
+    VariableExprNode *name;
+    ExprNode *expr;
+public:
+    VarDecStatementNode(VariableExprNode *type, VariableExprNode *name): type(type), name(name), expr(NULL) {}
+    VarDecStatementNode(VariableExprNode *type, VariableExprNode *name, ExprNode *expr): type(type), name(name), expr(expr) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class ArrayDecStatementNode : public StatementNode {
 public:
-    virtual Value *codegen();
+    VariableExprNode *type;
+    VariableExprNode *name;
+    vector<ExprNode*> *init;
+    long long size;
+    bool isString;
+public:
+    ArrayDecStatementNode(VariableExprNode *type, VariableExprNode *name, long long size): type(type), name(name), init(new vector<ExprNode*>()), size(size), isString(false) {}
+    ArrayDecStatementNode(VariableExprNode *type, VariableExprNode *name, vector<ExprNode*> *init): type(type), name(name), init(init), size(init->size()), isString(false) {}
+    ArrayDecStatementNode(VariableExprNode *type, VariableExprNode *name, const string &str): type(type), name(name), init(new vector<ExprNode*>()), isString(true) {
+        for(auto it = str.begin(); it != str.end(); it++)
+            init->push_back((ExprNode*)(new CharExprNode(*it)));
+        init->push_back((ExprNode*)(new CharExprNode(0)));
+        size = init->size() + 1;
+    }
+   virtual Value *CodeGen(GenContext&);
+    
 };
 
 class IndexExprNode : public ExprNode {
 public:
-    virtual Value *codegen();
+    VariableExprNode *name;
+    ExprNode *expr;
+    ExprNode *assign;
+public:
+    IndexExprNode(VariableExprNode *name, ExprNode *expr): name(name), expr(expr), assign(NULL) {}
+    IndexExprNode(VariableExprNode *name, ExprNode *expr, ExprNode *assign): name(name), expr(expr), assign(assign) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
-class FuncDecStatementNode : public StatementNode { 
+class FuncDecStatementNode : public StatementNode {
 public:
-    virtual Value *codegen();
+    VariableExprNode *type;
+    VariableExprNode *name;
+    vector<VarDecStatementNode*> *args;
+    BlockExprNode *block;
+public:
+    FuncDecStatementNode(VariableExprNode *type, VariableExprNode *name, vector<VarDecStatementNode*> *args, BlockExprNode *block): type(type), name(name), args(args), block(block) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class ExternFuncDecStatementNode : public StatementNode {
 public:
-    virtual Value *codegen();
+    VariableExprNode *type;
+    VariableExprNode *name;
+    vector<VarDecStatementNode*> *args;
+public:
+    ExternFuncDecStatementNode(VariableExprNode *type, VariableExprNode *name, vector<VarDecStatementNode*> *_args): type(type), name(name), args(_args) {
+        vector<VarDecStatementNode*>::const_iterator it;
+    }
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class IfStatementNode : public StatementNode {
 public:
-    virtual Value *codegen();
+    ExprNode *condExpr;
+    BlockExprNode *trueBlock;
+    BlockExprNode *falseBlock;
+public:
+    IfStatementNode(ExprNode *condExpr, BlockExprNode *trueBlock): condExpr(condExpr), trueBlock(trueBlock), falseBlock(new BlockExprNode()) {}
+    IfStatementNode(ExprNode *condExpr, BlockExprNode *trueBlock, BlockExprNode *falseBlock): condExpr(condExpr), trueBlock(trueBlock), falseBlock(falseBlock) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 class ForStatementNode : public StatementNode {
 public:
-    virtual Value *codegen();
+    ExprNode *initExpr;
+    ExprNode *condExpr;
+    ExprNode *loopExpr;
+    BlockExprNode *block;
+public:
+    ForStatementNode(ExprNode *initExpr, ExprNode *condExpr, ExprNode *loopExpr, BlockExprNode *block): initExpr(initExpr), condExpr(condExpr), loopExpr(loopExpr), block(block) {}
+    virtual Value *CodeGen(GenContext&);
+    
+};
+
+class WhileStatementNode : public StatementNode {
+public:
+    ExprNode *whileExpr;
+    BlockExprNode *block;
+public:
+    WhileStatementNode(ExprNode *whileExpr, BlockExprNode *block): whileExpr(whileExpr), block(block) {}
+    virtual Value *CodeGen(GenContext&);
+    
 };
 
 #endif
